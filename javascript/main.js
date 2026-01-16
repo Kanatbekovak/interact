115054ee9e2c46c1e29a9551e2541fdd2528ef68
Kanatbekovak <kanatbekovakaniet@gmail.com>
2026-01-16 22:16:51 +0600

    form

--- javascript/main.js
+++ javascript/main.js
@@ -18,3 +18,59 @@ items.forEach(item => {
 });
 
 
+
+////////////////////////////////////////////
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('invitationModal');
+    const openBtns = document.querySelectorAll('.my_help'); // Все кнопки хедере
+    const closeBtn = document.getElementById('closeModal');
+    const donateBtn = document.getElementById('donate');
+    const moneyBtns = document.querySelectorAll('.moneyfff');
+    const customSum = document.getElementById('custom_sum');
+    const resultsBtn = document.getElementById('.results_button');
+
+    // 1. ОТКРЫТИЕ ПРИ НАВЕДЕНИИ
+    openBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    
+
+    // 2. ЗАКРЫТИЕ
+    const closeModal = () => {
+        modal.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    };
+    closeBtn.onclick = closeModal;
+    document.getElementById('okey').onclick = closeModal;
+    window.onclick = (e) => { if (e.target == modal) closeModal(); };
+
+    // 3. ЛОГИКА СУММ
+    moneyBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            moneyBtns.forEach(b => b.classList.remove('selected'));
+            btn.classList.add('selected');
+            customSum.value = '';
+            donateBtn.innerText = `Пожертвовать ${btn.innerText}`;
+        });
+    });
+
+    customSum.oninput = () => {
+        moneyBtns.forEach(b => b.classList.remove('selected'));
+        donateBtn.innerText = `Пожертвовать ${customSum.value || 0} сом`;
+    };
+
+    // 4. ПЕРЕКЛЮЧЕНИЕ ШАГОВ
+    donateBtn.onclick = () => {
+        if(!document.getElementById('oferta').checked) return alert("Примите оферту");
+        
+        document.getElementById('step1').classList.remove('active');
+        document.getElementById('step2').classList.add('active');
+        document.getElementById('step1Dot').classList.remove('active');
+        document.getElementById('step2Dot').classList.add('active');
+    };
+});
